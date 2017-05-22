88b2f2614bae76967049926f09c47b7b7aa41b93
pnhung177 <pnhung177@acegik.net>
2017-05-22 12:05:15 +0700

    Update the exclusive/noAck configures

--- test/app/README.md
+++ test/app/README.md
@@ -7,3 +7,9 @@
 ```shell
 $ DEBUG=devebot*,app* NODE_DEVEBOT_PROFILE=console NODE_DEVEBOT_SANDBOX=test node test/app
 ```
+
+```
+sudo mount -o bind \
+	"/home/drupalex/projects/devebot/devebot" \
+	"/home/drupalex/projects/devebot/devebot-co-rabbitmq/node_modules/devebot"
+```
---
+++ test/cmd/publish_messages.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://master:zaq123edcx@192.168.56.56', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+  	ch.assertExchange('sample-exchange', 'direct', {
+  		durable: true
+  	});
+
+  	for(var i=0; i<1000; i++) {
+  		ch.publish('sample-exchange', 'sample', new Buffer('Messasge #' + i));
+  	}
+
+	console.log(" [x] Sent 'Hello World!'");    
+    ch.close(function(err) {
+    	console.log(" [x] Channel has been closed");
+    	conn.close(function(err) {
+    		console.log(" [x] Connection has been closed");
+    	});
+    });
+  });
+});
